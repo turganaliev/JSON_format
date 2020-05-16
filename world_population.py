dbd46fb39d3082e4fd2a4b997aeabe5b93d165c2
Izat Turganaliev <izatturganaliev@Izats-MacBook-Air.local>
2020-05-16 23:23:55 +0600

    data_loading

---
+++ world_population.py
@@ -0,0 +1,13 @@
+import json
+
+# Список заполняется данными
+filename = 'population_data.json'
+with open(filename) as f:
+    pop_data = json.load(f)
+
+# Вывод населения каждой страны за 2010 год
+for pop_dict in pop_data:
+    if pop_dict['Year'] == '2010':
+        country_name = pop_dict['Country Name']
+        population = int(float(pop_dict['Value']))
+        print(country_name + ': ' + str(population))
